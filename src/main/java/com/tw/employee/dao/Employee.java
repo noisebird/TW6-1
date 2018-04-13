a0f7eee71036be4f5ae56d3df2b8b0685b8a8f83
wangjie <728199993@qq.com>
2018-04-13 19:51:18 +0800

    add dao content

--- src/main/java/com/tw/employee/dao/Employee.java
+++ src/main/java/com/tw/employee/dao/Employee.java
@@ -4,5 +4,49 @@ package com.tw.employee.dao;
  * Created by wangjie on 2018/4/13.
  */
 public class Employee {
+    private String name;
+    private int id;
+    private  int age;
+    private String gender;
 
+    public Employee() {
+    }
+    public Employee(String name, int id, int age, String gender) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
