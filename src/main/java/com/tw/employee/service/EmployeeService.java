627c2e2e5634bc3d3a1d9300764e73b5e89d36a4
wangjie <728199993@qq.com>
2018-04-13 20:01:44 +0800

    add interface of service

--- src/main/java/com/tw/employee/service/EmployeeService.java
+++ src/main/java/com/tw/employee/service/EmployeeService.java
@@ -1,7 +1,16 @@
 package com.tw.employee.service;
 
+import com.tw.employee.dao.Employee;
+
+import java.util.List;
+
 /**
  * Created by wangjie on 2018/4/13.
  */
 public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    boolean addEmployee() ;
+    boolean deleteEmployee();
+    Employee getEmployeeById();
+    boolean updateEmployee();
 }
--- src/main/java/com/tw/employee/service/EmployeeServiceImpl.java
+++ src/main/java/com/tw/employee/service/EmployeeServiceImpl.java
@@ -1,7 +1,13 @@
 package com.tw.employee.service;
 
+import com.tw.employee.dao.Employee;
+
+import java.util.List;
+
 /**
  * Created by wangjie on 2018/4/13.
  */
 public class EmployeeServiceImpl implements EmployeeService {
+
+
 }
